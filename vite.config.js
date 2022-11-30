0af0ae778cc2b7ce8fd277881fa3fd5cd3de9cb9
Kyryl Savytskyi <kirill_savitsky56@gmail.com>
2022-11-30 22:24:36 +0200

    third commit add base

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  bash: /division-of-two-numbers-form/,
+  base: '/division-of-two-numbers-form/',
 })
